04f8bf5ff652f18adbfdc5a9ba8642eebcb6be7a
Luciba <luciano.srjunior@gmail.com>
2025-03-09 21:00:01 -0300

    change Bird image to matcholas

--- FlappyBird.py
+++ FlappyBird.py
@@ -7,10 +7,15 @@ GAME_HEIGHT = 800
 IMG_PIPE = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'pipe.png')))
 IMG_BASE = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'base.png')))
 IMG_BACCKGROUND = pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bg.png')))
+# IMGS_BIRD = [
+#     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
+#     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
+#     pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+# ]
 IMGS_BIRD = [
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird1.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird2.png'))),
-    pygame.transform.scale2x(pygame.image.load(os.path.join('imgs', 'bird3.png')))
+    pygame.image.load(os.path.join('imgs', 'Matcholas_bike.PNG')),
+    pygame.image.load(os.path.join('imgs', 'Matcholas_bike.PNG')),
+    pygame.image.load(os.path.join('imgs', 'Matcholas_bike.PNG'))
 ]
 IMG_APP = pygame.transform.scale(pygame.image.load(os.path.join('imgs', 'Background.jpg')),(500,800))
 
